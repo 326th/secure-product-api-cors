0cd5a0592bc5c776544ff96f5f5074f8827561bb
326th <minecraftskinman@gmail.com>
2021-05-18 21:32:19 +0700

    done

--- src/main/java/ku/product/config/SecurityConfig.java
+++ src/main/java/ku/product/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ku.product.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,21 +24,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/api/products")
+                .hasAuthority("SCOPE_read:products")
+                .mvcMatchers(HttpMethod.POST, "/api/products")
+                .hasAuthority("SCOPE_create:products")
                 .anyRequest()
                 .authenticated()
 
-                // use stateless session, so user's state is not stored
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-                // use oauth as a resource server to do jwt validation
                 .and()
                 .oauth2ResourceServer()
                 .jwt()
                 .decoder(jwtDecoder());
     }
+
 
     private JwtDecoder jwtDecoder() {
         OAuth2TokenValidator<Jwt> withAudience =
