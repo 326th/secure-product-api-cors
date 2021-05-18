021398ab07abd2887f7f17f0a1468bccc3dce8ab
326th <minecraftskinman@gmail.com>
2021-05-18 20:06:40 +0700

    fixes bugs

--- src/main/java/ku/product/model/Product.java
+++ src/main/java/ku/product/model/Product.java
@@ -15,6 +15,28 @@ public class Product {
     private String name;
     private double price;
 
-    // ... generate all getters / setters
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
